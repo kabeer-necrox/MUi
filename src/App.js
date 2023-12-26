519b85dc9990361ce85e9ee1a2dc6536c880bfdd
kabeer-necrox <khushkabeer@gmail.com>
2023-12-26 15:57:29 +0500

    hrthejjekeej

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import SearchIcon from '@mui/icons-material/Search';
+import ManageSearchIcon from '@mui/icons-material/ManageSearch';
+// import PolicyIcon from '@mui/icons-material/Policy';
+import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1>this is the react page</h1>
+  
+    <SearchIcon />
+
+    <ManageSearchIcon />
+   
+       <PersonSearchIcon />
+      
+
+    
+    </div>
+  );
+}
+
+export default App;
