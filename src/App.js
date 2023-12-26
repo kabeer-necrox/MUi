107dec88fc0cdd1824646430532bae0454ac393e
kabeer-necrox <khushkabeer@gmail.com>
2023-12-26 18:43:10 +0500

    new code of mui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import SearchIcon from "@mui/icons-material/Search";
+import ManageSearchIcon from "@mui/icons-material/ManageSearch";
+import PersonSearchIcon from "@mui/icons-material/PersonSearch";
+import Nav from "./components/Nav";
 
-import SearchIcon from '@mui/icons-material/Search';
-import ManageSearchIcon from '@mui/icons-material/ManageSearch';
-// import PolicyIcon from '@mui/icons-material/Policy';
-import PersonSearchIcon from '@mui/icons-material/PersonSearch';
+import * as React from 'react';
+import Avatar from '@mui/material/Avatar';
+import AvatarGroup from '@mui/material/AvatarGroup';
 
 
 
@@ -12,16 +14,50 @@ import PersonSearchIcon from '@mui/icons-material/PersonSearch';
 function App() {
   return (
     <div className="App">
-    <h1>this is the react page</h1>
-  
-    <SearchIcon />
+      <h1>this is the react page</h1>
 
-    <ManageSearchIcon />
-   
-       <PersonSearchIcon />
+      <SearchIcon />
+
+      <ManageSearchIcon />
+
+      <PersonSearchIcon />
+
+      <Nav />
+
+      <div className="ava">
+
+ 
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
       
+  
 
-    
+      </div>
+      <div>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+  <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+  <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+  <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+      </div>
+      <>
+      <AvatarGroup total={24}>
+      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
+      <Avatar alt="Agnes Walker" src="/static/images/avatar/4.jpg" />
+      <Avatar alt="Trevor Henderson" src="/static/images/avatar/5.jpg" />
+    </AvatarGroup></>
+
+    <Avatar
+  alt="Remy Sharp"
+  src="/static/images/avatar/1.jpg"
+  sx={{ width: 24, height: 24 }}
+/>
+<Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+<Avatar
+  alt="Remy Sharp"
+  src="/static/images/avatar/1.jpg"
+  sx={{ width: 56, height: 56 }}
+/>
     </div>
   );
 }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+
+const Nav=()=>{
+    return(
+        <>
+        <h1>this is the Navbar</h1>
+        </>
+    )
+}
+export default Nav;
